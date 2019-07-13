8bc7bf94368957ddf2e9020e328af78e87df3825
tomhosker <tomdothosker@gmail.com>
2019-07-13 18:46:36 +0100

    Finished Introduction to the Kingdom.

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.use(favicon(__dirname+"/public/favicon.ico"));
 
 // ROUTES.
